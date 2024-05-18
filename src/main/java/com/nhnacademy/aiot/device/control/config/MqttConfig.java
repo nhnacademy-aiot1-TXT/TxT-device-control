c95d0c1ce9ed974330673f4e0dc31621c580cb1f
pass0210 <pass0210@gmail.com>
2024-05-18 14:02:58 +0900

    Add: config 추가

---
+++ src/main/java/com/nhnacademy/aiot/device/control/config/MqttConfig.java
@@ -0,0 +1,55 @@
+package com.nhnacademy.aiot.device.control.config;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.handler.annotation.Header;
+
+@Configuration
+@IntegrationComponentScan
+public class MqttConfig {
+    private MqttConnectOptions connectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true);
+        options.setServerURIs(new String[]{"tcp://133.186.153.19:1883"});
+        return options;
+    }
+
+    @Bean
+    public DefaultMqttPahoClientFactory defaultMqttPahoClientFactory() {
+        DefaultMqttPahoClientFactory clientFactory = new DefaultMqttPahoClientFactory();
+        clientFactory.setConnectionOptions(connectOptions());
+        return clientFactory;
+    }
+
+    @Bean
+    public MessageChannel mqttOutboundChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutBound(DefaultMqttPahoClientFactory clientFactory) {
+        MqttPahoMessageHandler messageHandler =
+                new MqttPahoMessageHandler("txt", clientFactory);
+
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultQos(2);
+
+        return messageHandler;
+    }
+
+    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
+    public interface OutboundGateway {
+        void sendToMqtt(String payload, @Header(MqttHeaders.TOPIC) String topic);
+    }
+}
