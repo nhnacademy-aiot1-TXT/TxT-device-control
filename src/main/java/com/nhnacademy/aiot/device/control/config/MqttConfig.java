53fbae6894b102db54b401e86ec44d1256140a8b
pass0210 <pass0210@gmail.com>
2024-05-22 17:35:24 +0900

    Refactor: redis 상태 저장 수정 및 dooray message 전송 수정

--- src/main/java/com/nhnacademy/aiot/device/control/config/MqttConfig.java
+++ src/main/java/com/nhnacademy/aiot/device/control/config/MqttConfig.java
@@ -1,5 +1,15 @@
 package com.nhnacademy.aiot.device.control.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.aiot.device.control.adapter.DeviceSettingAdapter;
+import com.nhnacademy.aiot.device.control.dto.DeviceControlAck;
+import com.nhnacademy.aiot.device.control.dto.NotificationRequest;
+import com.nhnacademy.aiot.device.control.send.MessageSender;
+import com.nhnacademy.aiot.device.control.service.RedisService;
+import com.nhnacademy.aiot.device.control.util.CommonUtil;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +17,29 @@ import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.integration.mqtt.support.MqttHeaders;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.handler.annotation.Header;
 
 @Configuration
+@RequiredArgsConstructor
 @IntegrationComponentScan
 public class MqttConfig {
+    private static final String BROKER_URL = "tcp://133.186.153.19:1883";
+    private static final String MQTT_CLIENT_ID = MqttAsyncClient.generateClientId();
+    private static final String TOPIC_FILTER = "txt";
+    private static final String DEVICE_KEY = "device_power_status";
+    private static final Long USER_ROLE_ID = 2L;
+    private final RedisService redisService;
+    private final DeviceSettingAdapter deviceSettingAdapter;
+    private final MessageSender messageSender;
+
     private MqttConnectOptions connectOptions() {
         MqttConnectOptions options = new MqttConnectOptions();
         options.setCleanSession(true);
@@ -51,5 +74,45 @@ public class MqttConfig {
     @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
     public interface OutboundGateway {
         void sendToMqtt(String payload, @Header(MqttHeaders.TOPIC) String topic);
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inboundChannel() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(BROKER_URL, MQTT_CLIENT_ID, TOPIC_FILTER);
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler inboundMessageHandler() {
+        return message -> {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                DeviceControlAck deviceControlAck = objectMapper.readValue((String) message.getPayload(), DeviceControlAck.class);
+                redisService.setDeviceStatus(DEVICE_KEY, deviceControlAck.getDevice().concat(":").concat(deviceControlAck.getPlace()), deviceControlAck.getValue());
+
+                NotificationRequest notificationRequest = CommonUtil.createDeviceControlNotification(USER_ROLE_ID, redisService.getPlaceName(deviceControlAck.getPlace()), deviceControlAck.getDevice(), deviceControlAck.getValue());
+                deviceSettingAdapter.addNotification(notificationRequest);
+
+                sendMessage(deviceControlAck.getDevice() + "이", deviceControlAck);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private void sendMessage(String device, DeviceControlAck deviceControlAck) {
+        String status = deviceControlAck.getValue() ? " 켜졌습니다." : " 꺼졌습니다.";
+        messageSender.send(device.substring(0, device.length() - 1) + " 봇", redisService.getPlaceName(deviceControlAck.getPlace()) + "의 " + device + status);
     }
 }
--- src/main/java/com/nhnacademy/aiot/device/control/service/MessageListener.java
+++ src/main/java/com/nhnacademy/aiot/device/control/service/MessageListener.java
@@ -22,9 +22,8 @@ public class MessageListener {
     private static final String LIGHT = "light";
     private static final String LIGHT_QUEUE = "txt.light.queue";
     private static final String INTRUSION_QUEUE = "txt.intrusion.queue";
-    private static final String DEVICE_KEY = "device_power_status";
+
     private static final Long ADMIN_ROLE_ID = 1L;
-    private static final Long USER_ROLE_ID = 2L;
     private final OutboundGateway outboundGateway;
     private final RedisService redisService;
     private final DeviceSettingAdapter deviceSettingAdapter;
@@ -33,34 +32,16 @@ public class MessageListener {
     @RabbitListener(queues = AIR_CONDITIONER_QUEUE)
     public void airConditionerProcess(ValueMessage message) {
         outboundGateway.sendToMqtt(message.getValue().toString(), CommonUtil.createDeviceControlTopic(AIR_CONDITIONER, message.getPlace()));
-        redisService.setDeviceStatus(DEVICE_KEY, AIR_CONDITIONER.concat(":").concat(message.getPlace()), message.getValue());
-
-        NotificationRequest notificationRequest = CommonUtil.createDeviceControlNotification(USER_ROLE_ID, redisService.getPlaceName(message.getPlace()), AIR_CONDITIONER, message.getValue());
-        deviceSettingAdapter.addNotification(notificationRequest);
-
-        sendMessage("에어컨이", message);
     }
 
     @RabbitListener(queues = AIR_CLEANER_QUEUE)
     public void airCleanerProcess(ValueMessage message) {
         outboundGateway.sendToMqtt(message.getValue().toString(), CommonUtil.createDeviceControlTopic(AIR_CLEANER, message.getPlace()));
-        redisService.setDeviceStatus(DEVICE_KEY, AIR_CLEANER.concat(":").concat(message.getPlace()), message.getValue());
-
-        NotificationRequest notificationRequest = CommonUtil.createDeviceControlNotification(USER_ROLE_ID, redisService.getPlaceName(message.getPlace()), AIR_CLEANER, message.getValue());
-        deviceSettingAdapter.addNotification(notificationRequest);
-
-        sendMessage("공기 청정기가", message);
     }
 
     @RabbitListener(queues = LIGHT_QUEUE)
     public void lightProcess(ValueMessage message) {
         outboundGateway.sendToMqtt(message.getValue().toString(), CommonUtil.createDeviceControlTopic(LIGHT, message.getPlace()));
-        redisService.setDeviceStatus(DEVICE_KEY, LIGHT.concat(":").concat(message.getPlace()), message.getValue());
-
-        NotificationRequest notificationRequest = CommonUtil.createDeviceControlNotification(USER_ROLE_ID, redisService.getPlaceName(message.getPlace()), LIGHT, message.getValue());
-        deviceSettingAdapter.addNotification(notificationRequest);
-
-        sendMessage("조명이", message);
     }
 
     @RabbitListener(queues = INTRUSION_QUEUE)
@@ -70,10 +51,5 @@ public class MessageListener {
 
         String status = message.getValue() ? " 작동했습니다." : " 정지했습니다.";
         messageSender.send("침입감지 봇", redisService.getPlaceName(message.getPlace()) + "의" + "침입감지가" + status);
-    }
-
-    private void sendMessage(String device, ValueMessage message) {
-        String status = message.getValue() ? " 켜졌습니다." : " 꺼졌습니다.";
-        messageSender.send(device.substring(0, device.length() - 1) + " 봇", redisService.getPlaceName(message.getPlace()) + "의 " + device + status);
     }
 }
